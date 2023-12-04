ad3c9e767405df92212a1dc075b9c4c58d5ed493
welt-03 <125975634+welt-03@users.noreply.github.com>
2023-12-04 22:03:38 +0800

    update lvgl

--- main/main.c
+++ main/main.c
@@ -17,7 +17,7 @@
 #include "lvgl_helpers.h"
 
 #define TAG "main"
-#define LV_TICK_PERIOD_MS 10
+#define LV_TICK_PERIOD_MS 1
 #define LED_GREEN GPIO_NUM_15
 #define LED_YELLOW GPIO_NUM_16
 
@@ -26,10 +26,15 @@
 #define BUTTON_UP GPIO_NUM_10
 #define BUTTON_MENU GPIO_NUM_14
 
+#define HIGH 1
+#define LOW 0
+
 #define BUF_SIZE 1024
 
 static QueueHandle_t gpio_evt_queue = NULL;
 static uint8_t key_state;
+static lv_disp_t* disp = NULL;
+static lv_indev_t* indev_keypad = NULL;
 
 static void gpio_task_handler(void* arg) {
     uint32_t io_num;
@@ -40,24 +45,27 @@ static void gpio_task_handler(void* arg) {
             switch (io_num) {
                 case BUTTON_OK:
                     key_state = LV_KEY_ENTER;
-                    gpio_set_level(LED_GREEN, !gpio_get_level((gpio_num_t)io_num));
-                    gpio_set_level(LED_YELLOW, !gpio_get_level((gpio_num_t)io_num));
+                    gpio_set_level(LED_GREEN, HIGH);
+                    gpio_set_level(LED_YELLOW, HIGH);
                     break;
                 case BUTTON_DW:
                     key_state = LV_KEY_DOWN;
-                    gpio_set_level(LED_GREEN, !gpio_get_level((gpio_num_t)io_num));
+                    gpio_set_level(LED_GREEN, HIGH);
                     break;
                 case BUTTON_UP:
                     key_state = LV_KEY_UP;
-                    gpio_set_level(LED_YELLOW, !gpio_get_level((gpio_num_t)io_num));
+                    gpio_set_level(LED_YELLOW, HIGH);
                     break;
                 case BUTTON_MENU:
-                    key_state = LV_KEY_ESC;
+                    key_state = LV_KEY_NEXT;
                     break;
                 default:
                     break;
             }
+            vTaskDelay(100 / portTICK_PERIOD_MS);
             gpio_intr_enable((gpio_num_t)io_num);
+            gpio_set_level(LED_GREEN, LOW);
+            gpio_set_level(LED_YELLOW, LOW);
         }
     }
 }
@@ -101,7 +109,7 @@ static void gpio_init() {
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_ANYEDGE,
+        .intr_type = GPIO_INTR_NEGEDGE,
     };
     gpio_config(&io_conf);
     io_conf.pin_bit_mask = BIT64(LED_GREEN) | BIT64(LED_YELLOW);
@@ -119,8 +127,7 @@ static void gpio_init() {
     gpio_isr_handler_add(BUTTON_OK, gpio_isr_handler, (void*)BUTTON_OK);
 }
 
-void app_main() {
-    gpio_init();
+static void lvgl_init() {
     lv_init();
     lvgl_driver_init();
 
@@ -139,25 +146,45 @@ void app_main() {
     disp_drv.draw_buf = &disp_buf;
     disp_drv.hor_res = LV_HOR_RES_MAX;
     disp_drv.ver_res = LV_VER_RES_MAX;
-    lv_disp_t* disp = lv_disp_drv_register(&disp_drv);  // 注册显示驱动
+    disp = lv_disp_drv_register(&disp_drv);  // 注册显示驱动
     // 配置输入设备
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.disp = disp;
     indev_drv.type = LV_INDEV_TYPE_KEYPAD;
     indev_drv.read_cb = lv_keypad_read;
-    lv_indev_t* indev_keypad = lv_indev_drv_register(&indev_drv);
+    indev_keypad = lv_indev_drv_register(&indev_drv);
 
     xTaskCreate(lv_tick_task, "lv_tick", BUF_SIZE, NULL, 10, NULL);
+}
 
+static void lvgl_app() {
     lv_obj_t* scr = lv_disp_get_scr_act(NULL);
     lv_obj_t* label = lv_label_create(scr);
     lv_obj_t* slider = lv_slider_create(scr);
+    lv_obj_t* btn = lv_spinner_create(scr, 1000, 40);
+    lv_obj_t* bar = lv_switch_create(scr);
     lv_group_t* group = lv_group_create();
+
+    lv_obj_set_x(btn, 100);
+    lv_obj_set_y(btn, 50);
+    lv_obj_set_x(bar, 100);
+    lv_obj_set_y(bar, 25);
+    lv_obj_set_style_align(slider, LV_ALIGN_CENTER, LV_STATE_DEFAULT);
+    lv_obj_set_style_width(slider, 100, LV_STATE_DEFAULT);
+    lv_obj_set_style_height(slider, 20, LV_STATE_DEFAULT);
+    lv_group_add_obj(group, bar);
     lv_group_add_obj(group, slider);
     lv_indev_set_group(indev_keypad, group);
     lv_obj_set_style_text_color(label, lv_color_hex(0xf50057), LV_STATE_DEFAULT);
     lv_label_set_text(label, "Hello World");
+}
+
+void app_main() {
+    gpio_init();
+    lvgl_init();
+
+    lvgl_app();
 
     while (1) {
         vTaskDelay(5 / portTICK_PERIOD_MS);
