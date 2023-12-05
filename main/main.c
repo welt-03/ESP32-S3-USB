aa9c15346dc0c628547ddec11c9f67fa0bc32d1b
welt-03 <1557850953@qq.com>
2023-12-05 17:02:39 +0800

    update main.c

--- main/main.c
+++ main/main.c
@@ -31,42 +31,76 @@
 
 #define BUF_SIZE 1024
 
+typedef struct {
+    uint8_t num;     // IO号
+    uint8_t clicks;  // 点击次数
+    uint8_t state;   // 按键状态
+} keypad_t;
+static keypad_t keypad;
+static esp_timer_handle_t timer_handle = NULL;
 static QueueHandle_t gpio_evt_queue = NULL;
-static uint8_t key_state;
 static lv_disp_t* disp = NULL;
 static lv_indev_t* indev_keypad = NULL;
 
 static void gpio_task_handler(void* arg) {
+    uint8_t level;
     uint32_t io_num;
+    TickType_t tick;
 
     while (true) {
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
             // ESP_LOGI(TAG, "GPIO[%" PRIu32 "] intr, val: %d", io_num, gpio_get_level((gpio_num_t)io_num));
+            level = gpio_get_level(io_num);
+            if (!level) {
+                esp_timer_start_periodic(timer_handle, 50000);
+                tick = xTaskGetTickCount();
+            } else {
+                esp_timer_stop(timer_handle);
+            }
             switch (io_num) {
                 case BUTTON_OK:
-                    key_state = LV_KEY_ENTER;
-                    gpio_set_level(LED_GREEN, HIGH);
-                    gpio_set_level(LED_YELLOW, HIGH);
+                    gpio_set_level(LED_GREEN, !level);
+                    gpio_set_level(LED_YELLOW, !level);
                     break;
                 case BUTTON_DW:
-                    key_state = LV_KEY_DOWN;
-                    gpio_set_level(LED_GREEN, HIGH);
+                    gpio_set_level(LED_GREEN, !level);
                     break;
                 case BUTTON_UP:
-                    key_state = LV_KEY_UP;
-                    gpio_set_level(LED_YELLOW, HIGH);
+                    gpio_set_level(LED_YELLOW, !level);
                     break;
                 case BUTTON_MENU:
-                    key_state = LV_KEY_NEXT;
+                    gpio_set_level(LED_GREEN, !level);
+                    gpio_set_level(LED_YELLOW, !level);
                     break;
                 default:
                     break;
             }
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-            gpio_intr_enable((gpio_num_t)io_num);
-            gpio_set_level(LED_GREEN, LOW);
-            gpio_set_level(LED_YELLOW, LOW);
         }
+    }
+}
+
+static void keypad_cb(void* arg) {
+    static uint8_t count = 0;
+
+    switch (keypad.num) {
+        case BUTTON_OK:
+            keypad.state = LV_KEY_ENTER;
+            // gpio_set_level(LED_GREEN, HIGH);
+            // gpio_set_level(LED_YELLOW, HIGH);
+            break;
+        case BUTTON_DW:
+            keypad.state = LV_KEY_DOWN;
+            // gpio_set_level(LED_GREEN, HIGH);
+            break;
+        case BUTTON_UP:
+            keypad.state = LV_KEY_UP;
+            // gpio_set_level(LED_YELLOW, HIGH);
+            break;
+        case BUTTON_MENU:
+            keypad.state = LV_KEY_NEXT;
+            break;
+        default:
+            break;
     }
 }
 
@@ -78,7 +112,7 @@ static void gpio_task_handler(void* arg) {
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
     uint32_t io_num = (uint32_t)arg;
 
-    gpio_intr_disable((gpio_num_t)io_num);
+    // gpio_intr_disable((gpio_num_t)io_num);
     xQueueSendFromISR(gpio_evt_queue, &io_num, NULL);
 }
 static void lv_tick_task(void* arg) {
@@ -93,33 +127,42 @@ static void lv_tick_task(void* arg) {
 static void lv_keypad_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data) {
     static uint8_t last_key = 0;
 
-    if (key_state) {
+    if (keypad.state) {
         data->state = LV_INDEV_STATE_PR;
-        last_key = key_state;
+        last_key = keypad.state;
     } else {
         data->state = LV_INDEV_STATE_REL;
     }
-    key_state = 0;
+    keypad.state = 0;
     data->key = last_key;
 }
 
-static void gpio_init() {
+static void keypad_init() {
     gpio_config_t io_conf = {
         .pin_bit_mask = BIT64(BUTTON_OK) | BIT64(BUTTON_DW) | BIT64(BUTTON_UP) | BIT64(BUTTON_MENU),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_NEGEDGE,
+        .intr_type = GPIO_INTR_ANYEDGE,
     };
     gpio_config(&io_conf);
     io_conf.pin_bit_mask = BIT64(LED_GREEN) | BIT64(LED_YELLOW);
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&io_conf);
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    gpio_evt_queue = xQueueCreate(5, sizeof(uint32_t));
     xTaskCreate(gpio_task_handler, "gpio_task", BUF_SIZE, NULL, 10, NULL);
 
     gpio_install_isr_service(0);
+
+    esp_timer_create_args_t timer_args = {
+        .callback = keypad_cb,
+        .arg = NULL,
+        .dispatch_method = ESP_TIMER_TASK,
+        .name = "keypad_cb",
+    };
+
+    ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer_handle));
 
     gpio_isr_handler_add(BUTTON_DW, gpio_isr_handler, (void*)BUTTON_DW);
     gpio_isr_handler_add(BUTTON_UP, gpio_isr_handler, (void*)BUTTON_UP);
@@ -181,7 +224,7 @@ static void lvgl_app() {
 }
 
 void app_main() {
-    gpio_init();
+    keypad_init();
     lvgl_init();
 
     lvgl_app();
